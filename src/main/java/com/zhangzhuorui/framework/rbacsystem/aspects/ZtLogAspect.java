fe9884c2b3ed71cd55bb4e3d4bb6e9add841f2ed
zhangtao <17091648421@126.com>
2021-11-07 10:18:38 +0800

    增加日志切面，打印请求参数

---
+++ src/main/java/com/zhangzhuorui/framework/rbacsystem/aspects/ZtLogAspect.java
@@ -0,0 +1,57 @@
+package com.zhangzhuorui.framework.rbacsystem.aspects;
+
+import com.alibaba.fastjson.JSON;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import java.lang.reflect.Method;
+import java.util.Enumeration;
+
+/**
+ * @author :  zhangtao
+ * @createDate :  2021/8/21 上午10:34
+ * @return :
+ * @description :  日志切面
+ * @updateUser :
+ * @updateDate :
+ * @updateRemark :
+ */
+@Component
+@Aspect
+@Slf4j
+@Order(-5)
+@CrossOrigin
+public class ZtLogAspect {
+
+    @Pointcut("execution(public * com.zhangzhuorui..*.controller..*.*(..))")
+    public void ztLog() {
+    }
+
+    @Before("ztLog()")
+    public void before(JoinPoint joinPoint) throws Exception {
+        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        HttpServletRequest request = attributes.getRequest();
+
+        Object[] args = joinPoint.getArgs();
+        MethodSignature signature = (MethodSignature) joinPoint.getSignature();
+        Method method = signature.getMethod();
+        log.info("{}.{} : 请求参数:{}", joinPoint.getTarget().getClass(), joinPoint.getSignature().getName(), JSON.toJSONString(args));
+
+        Enumeration<String> enu = request.getParameterNames();
+        while (enu.hasMoreElements()) {
+            String s = enu.nextElement();
+            log.info("具体参数：" + s + ":" + request.getParameter(s));
+        }
+    }
+
+}
